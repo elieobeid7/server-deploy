2323b1a567fb46c422dfc21be988ecb6366623e6
Elie Obeid <elieobeid7@gmail.com>
2020-06-11 10:58:27 +0300

    check if the file exists, otherwise delete

--- deploy.pl
+++ deploy.pl
@@ -23,6 +23,8 @@ foreach my $repo (@repos) {
             foreach my $line (@diff_output) {
                     $line =~ s/\s+//g;
                     my($filename, $directories, $suffix) = fileparse($git_files[0],qr"\..[^.]*$");
+                    my $git_file_path = $directories . $filename . $suffix;
+                    if (-e $git_file_path){ 
                         my $server_dir = $item->{path} . $directories;
                         eval { make_path($server_dir) };
                         if ($@) {
@@ -30,7 +32,7 @@ foreach my $repo (@repos) {
                         }
                         my $filepath = $directories . $filename . $suffix;
                         copy($filepath, $server_dir) or die "Failed to copy $filepath: $!\n"; 
-                        
+            }
                 }
         }
     }
