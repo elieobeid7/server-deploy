491a8ad43431e7f00b065795fa169279bab60565
Elie Obeid <elieobeid7@gmail.com>
2020-07-25 20:00:12 +0300

    fixing errors

--- deploy.pl
+++ deploy.pl
@@ -49,7 +49,7 @@ foreach my $repo (@repos) {
                         }
 
             }
-                    elseif ($delete_files==1) {
+                    elsif ($delete_files==1) {
                         $filepath = $directories . $filename . $suffix;
                         unlink $filepath or warn $!;
             }
