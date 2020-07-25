05bf32fff441cf0a59328f14aa9742e6510099c4
Elie Obeid <elieobeid7@gmail.com>
2020-07-25 18:32:03 +0300

    final commit

--- config.pm
+++ config.pm
@@ -5,14 +5,12 @@ use warnings;
 our @repos = (
 	{
 		repo_path   => "/root/dev/api_repo",
-		backup_path => "/var/www/html/backup",
-		log_path    => "/var/www/html/logs/",
+
 		branches    => [
 			{
                 branch_name => "stable",
                 copy_to_path => "/var/www/html/",
                 delete_files => 1,
-
                 ignore_files => [ 
                     ".gitignore"
                 ]
