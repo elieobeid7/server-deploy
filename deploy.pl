04bae5877226b5b73c9593dd92af0551e06ded96
Elie Obeid <elieobeid7@gmail.com>
2020-07-26 00:09:27 +0300

    fixing errors

--- deploy.pl
+++ deploy.pl
@@ -25,7 +25,7 @@ foreach my $repo (@repos) {
             
             # if the repo is not up to date, get the changed files as an array
 
-            my $git_dif_head_output =qx{git diff --name-only HEAD^1..HEAD};
+            my $git_dif_head_output = qx{git diff --name-only HEAD^1..HEAD};
             my @diff_output = split m/\r?\n/, $git_dif_head_output;
 
             foreach my $output_line (@diff_output) {
